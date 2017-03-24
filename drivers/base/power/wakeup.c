9193c61e4ae4dd192c7f663cdc8f4467428f5f50
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-24 01:51:22 +0000

    drivers: wakeup: it's pointless to output the active wakeup sources during screen on, no need to go through the rcu locks and list iterations every now and then
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    (cherry picked from commit bbc22936d662b886688ecec70b640e0a35800e85)
    (cherry picked from commit 508fbe7cf8adb6654da2a328990de5b621ff6bc0)
    (cherry picked from commit 4addc578305e607c96f072c2f3df9f3c2338c497)
    (cherry picked from commit 9507767f1c24d10164519e91c4bb331b32200958)
    (cherry picked from commit 143b76658dc6ff6382184fcc33fd87b6a81455e8)

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -891,7 +891,11 @@ void pm_print_active_wakeup_sources(void)
 	int srcuidx, active = 0;
 	struct wakeup_source *last_activity_ws = NULL;
 
-	srcuidx = srcu_read_lock(&wakeup_srcu);
+	// kinda pointless to force this routine during screen on
+	if (is_display_on())
+		return;
+
+	rcu_read_lock();
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
 			pr_info("active wakeup source: %s\n", ws->name);
