0064af1cab4cfa020a136ab242d3258b009f82a3
RenderBroken <zkennedy87@gmail.com>
2017-07-04 15:03:16 -0400

    include/linux/sched.h: Add back dump_fd_leak
    Signed-off-by: RenderBroken <zkennedy87@gmail.com>
    (cherry picked from commit 26a4c22611d53ef02a6f32ba0035a5cec532167b)

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1524,6 +1524,9 @@ struct task_struct {
 	atomic_t usage;
 	unsigned int flags;	/* per process flags, defined below */
 	unsigned int ptrace;
+	
+//xiaoxiaohuan add for fd leak debug
+    bool dump_fd_leak;
 
 #ifdef CONFIG_SMP
 	struct llist_node wake_entry;
