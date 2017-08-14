derkling/linux

DEBUG: schedtune: add tracepoint for CPU boost signal
Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

include/trace/events/sched.h
@@ -706,6 +706,33 @@ TRACE_EVENT(sched_boost_task,
 		  __entry->margin)
 );
 
+/*
+ * Tracepoint for accounting CPU  boosted utilization
+ */
+TRACE_EVENT(sched_boost_cpu,
+
+	TP_PROTO(int cpu, unsigned long usage, unsigned long margin),
+
+	TP_ARGS(cpu, usage, margin),
+
+	TP_STRUCT__entry(
+		__field( int,		cpu			)
+		__field( unsigned long,	usage			)
+		__field( unsigned long,	margin			)
+	),
+
+	TP_fast_assign(
+		__entry->cpu	= cpu;
+		__entry->usage	= usage;
+		__entry->margin	= margin;
+	),
+
+	TP_printk("cpu=%d usage=%lu margin=%lu",
+		  __entry->cpu,
+		  __entry->usage,
+		  __entry->margin)
+);
+
 #endif /* _TRACE_SCHED_H */
 
 /* This part must be outside protection */

kernel/sched/fair.c
@@ -5507,6 +5507,8 @@ get_boosted_cpu_usage(int cpu)
 		return SCHED_LOAD_SCALE;
 	margin = schedtune_cpu_margin(cpu, capacity);
 
+	trace_sched_boost_cpu(cpu, capacity, margin);
+
 	capacity += margin;
 	return capacity;
 }

freak07/OCEAN_EAS

(cherry picked from commit d1452e38ad32c0f3b078f9c33d642a95e66ac7c4)

include/trace/events/sched.h
@@ -675,6 +675,33 @@ TRACE_EVENT(sched_tune_config,
 	TP_printk("boost=%d ", __entry->boost)
 );
 
+/*
+ * Tracepoint for accounting CPU  boosted utilization
+ */
+TRACE_EVENT(sched_boost_cpu,
+
+	TP_PROTO(int cpu, unsigned long util, unsigned long margin),
+
+	TP_ARGS(cpu, util, margin),
+
+	TP_STRUCT__entry(
+		__field( int,		cpu			)
+		__field( unsigned long,	util			)
+		__field( unsigned long,	margin			)
+	),
+
+	TP_fast_assign(
+		__entry->cpu	= cpu;
+		__entry->util	= util;
+		__entry->margin	= margin;
+	),
+
+	TP_printk("cpu=%d util=%lu margin=%lu",
+		  __entry->cpu,
+		  __entry->util,
+		  __entry->margin)
+);
+
 #endif /* _TRACE_SCHED_H */
 
 /* This part must be outside protection */

kernel/sched/fair.c
@@ -5272,6 +5272,8 @@ boosted_cpu_util(int cpu)
 	unsigned long util = cpu_util(cpu);
 	unsigned long margin = schedtune_cpu_margin(util, cpu);
 
+	trace_sched_boost_cpu(cpu, util, margin);
+
 	return util + margin;
 }
 